4e87be83a4a2f7424c0ce2d23624ed3928ad1c33
Paco Garcia <koliko@gmail.com>
2019-11-11 19:36:30 +0100

    Configurando Rails, Vue y Buefy

---
+++ app/javascript/app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Visitas
+          </h1>
+          <h2 class="subtitle">
+            Interface para controlar las visitas en la aplicación Rails
+          </h2>
+        </div>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Di hola a Vue .."
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
